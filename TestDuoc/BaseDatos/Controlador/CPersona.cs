0a2b173b55a4bccb716619de1d8acf998bc252e1
Alfonso-Chiguay <alfonso.chiguay@gmail.com>
2020-06-27 16:38:11 -0400

    Se genera una funcion basica de busqueda de persona en base de datos

---
+++ TestDuoc/BaseDatos/Controlador/CPersona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDatos.Controlador
+{
+    public class CPersona
+    {
+       public string obtenerNombre(int rut)
+        {
+            using(test_duocEntities entidades = new test_duocEntities())
+            {
+                var consulta = entidades.persona.Where(x => x.rut == rut).FirstOrDefault();
+                return consulta.nombre;
+            }
+        }
+        
+
+    }
+}
--- TestDuoc/TestDuoc/MainWindow.xaml.cs
+++ TestDuoc/TestDuoc/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BaseDatos.Controlador;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,12 @@ namespace TestDuoc
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_buscar_Click(object sender, RoutedEventArgs e)
+        {
+            CPersona controlador = new CPersona();
+            lbl_nombre.Content = controlador.obtenerNombre(Int32.Parse(txt_rut.Text));
         }
     }
 }
